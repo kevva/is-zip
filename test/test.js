bc2792cc8eb46628530a7c87cd5d678b87fc199d
Kevin Martensson <kevinmartensson@gmail.com>
2014-04-22 15:45:03 +0200

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,23 @@
+/*global describe, it */
+'use strict';
+
+var assert = require('assert');
+var fs = require('fs');
+var isZip = require('../');
+var path = require('path');
+
+describe('extname()', function () {
+    function check(file) {
+        return isZip(fs.readFileSync(file));
+    }
+
+    it('should detect ZIP from buffer', function (cb) {
+        assert(check(path.join(__dirname, 'fixtures/test.zip')));
+        cb();
+    });
+
+    it('should detect ZIP from Uint8Array', function () {
+        var buf = new Uint8Array([80, 75, 3, 4]);
+        assert(isZip(buf));
+    });
+});
